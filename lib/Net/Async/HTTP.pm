69ba1e9d1de17f334f3c9221cae69b9f5e32f673
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2008-05-12 16:18:42 +0100

    Rewrote to use IO::Async::Sequencer

--- lib/Net/Async/HTTP.pm
+++ lib/Net/Async/HTTP.pm
@@ -11,13 +11,12 @@ our $VERSION = '0.01';
 
 use Carp;
 
+use Net::Async::HTTP::Client;
+
 use HTTP::Request;
-use HTTP::Response;
 use HTTP::Request::Common qw();
 
 use Socket qw( SOCK_STREAM );
-
-my $CRLF = "\x0d\x0a"; # More portable than \r\n
 
 =head1 NAME
 
@@ -129,35 +128,11 @@ sub get_connection
 
       my $cr;
 
-      my $conn = IO::Async::Stream->new(
+      my $conn = Net::Async::HTTP::Client->new(
          handle => $args{handle},
 
-         on_read => sub {
-            my ( $conn, $buffref, $closed ) = @_;
-
-            if( defined $on_read ) {
-               my $r = $on_read->( $conn, $buffref, $closed );
-               if( ref $r eq "CODE" ) {
-                  $on_read = $r;
-                  return 1;
-               }
-               elsif( defined $r ) {
-                  return $r;
-               }
-               else {
-                  if( @$cr > 1 ) {
-                     $on_read = splice @$cr, 1, 1;
-                     return 1;
-                  }
-
-                  $conn->close;
-                  delete $self->{connections}->{"$host:$port"};
-                  return 0;
-               }
-            }
-            else {
-               die "Spurious on_read of connection while idle\n";
-            }
+         on_closed => sub {
+            delete $self->{connections}->{"$host:$port"};
          },
       );
 
@@ -333,10 +308,9 @@ sub do_request
 
          on_ready => sub {
             my ( $conn ) = @_;
-            return $self->do_request_conn(
+            $conn->request(
                %args,
                request => $request,
-               conn    => $conn,
             );
          },
       );
@@ -366,123 +340,6 @@ sub do_request
          },
       );
    }
-}
-
-sub do_request_conn
-{
-   my $self = shift;
-   my %args = @_;
-
-   my $on_response = $args{on_response} or croak "Expected 'on_response' as a CODE ref";
-   my $on_error    = $args{on_error}    or croak "Expected 'on_error' as a CODE ref";
-   
-   my $req = $args{request};
-   ref $req and $req->isa( "HTTP::Request" ) or croak "Expected 'request' as a HTTP::Request reference";
-
-   my $method = $req->method;
-
-   my $conn = $args{conn};
-
-   if( $method eq "POST" or $method eq "PUT" or length $req->content ) {
-      $req->init_header( "Content-Length", length $req->content );
-   }
-
-   # HTTP::Request is silly and uses "\n" as a separator. We must tell it to
-   # use the correct RFC 2616-compliant CRLF sequence.
-   $conn->write( $req->as_string( $CRLF ) );
-
-   # Now construct the on_read closure
-   return sub {
-      my ( $conn, $buffref, $closed ) = @_;
-
-      unless( $$buffref =~ s/^(.*?$CRLF$CRLF)//s ) {
-         $on_error->( "Connection closed while awaiting header" ) if $closed;
-         return 0;
-      }
-
-      my $response_header = $1;
-      my $response = HTTP::Response->parse( $response_header );
-
-      my $code = $response->code;
-
-      # RFC 2616 says "HEAD" does not have a body, nor do any 1xx codes, nor
-      # 204 (No Content) nor 304 (Not Modified)
-      if( $method eq "HEAD" or $code =~ m/^1..$/ or $code eq "204" or $code eq "304" ) {
-         $on_response->( $response );
-         return undef; # Finished
-      }
-
-      my $transfer_encoding = $response->header( "Transfer-Encoding" );
-      my $content_length = $response->content_length;
-
-      if( defined $transfer_encoding and $transfer_encoding eq "chunked" ) {
-         my $chunk_length;
-
-         return sub {
-            my ( $conn, $buffref, $closed ) = @_;
-
-            if( !defined $chunk_length and $$buffref =~ s/^(.*?)$CRLF// ) {
-               # Chunk header
-               $chunk_length = hex( $1 );
-               return 1 if $chunk_length;
-
-               $on_response->( $response );
-               return undef; # Finished
-            }
-
-            if( defined $chunk_length and length( $$buffref ) >= $chunk_length ) {
-               # Chunk body
-               my $chunk = substr( $$buffref, 0, $chunk_length, "" );
-               undef $chunk_length;
-
-               $response->add_content( $chunk );
-
-               return 1;
-            }
-
-            $on_error->( "Connection closed while awaiting chunk" ) if $closed;
-            return 0;
-         };
-      }
-      elsif( defined $content_length ) {
-         if( $content_length == 0 ) {
-            $on_response->( $response );
-            return undef; # Finished
-         }
-
-         return sub {
-            my ( $conn, $buffref, $closed ) = @_;
-
-            if( length $$buffref >= $content_length ) {
-               my $content = substr( $$buffref, 0, $content_length, "" );
-
-               $response->content( $content );
-
-               $on_response->( $response );
-               return undef; # Finished
-            }
-
-            $on_error->( "Connection closed while awaiting body" ) if $closed;
-            return 0;
-         };
-      }
-      else {
-         return sub {
-            my ( $conn, $buffref, $closed ) = @_;
-
-            return 0 unless $closed;
-
-            my $content = $$buffref;
-            $$buffref = "";
-
-            $response->content( $content );
-
-            $on_response->( $response );
-            # $conn already closed
-            return undef;
-         };
-      }
-   };
 }
 
 # Keep perl happy; keep Britain tidy
---
+++ lib/Net/Async/HTTP/Client.pm
@@ -0,0 +1,166 @@
+#  You may distribute under the terms of either the GNU General Public License
+#  or the Artistic License (the same terms as Perl itself)
+#
+#  (C) Paul Evans, 2008 -- leonerd@leonerd.org.uk
+
+package Net::Async::HTTP::Client;
+
+use strict;
+
+our $VERSION = '0.01';
+
+use Carp;
+
+use base qw( IO::Async::Sequencer );
+
+use HTTP::Response;
+
+my $CRLF = "\x0d\x0a"; # More portable than \r\n
+
+sub new
+{
+   my $class = shift;
+   my %args = @_;
+
+   $args{marshall_request} = \&marshall_request;
+   $args{on_read}          = \&on_read;
+
+   my $self = $class->SUPER::new( %args );
+
+   return $self;
+}
+
+# For passing in to constructor
+sub marshall_request
+{
+   my $self = shift;
+   my ( $request ) = @_;
+
+   # HTTP::Request is silly and uses "\n" as a separator. We must tell it to
+   # use the correct RFC 2616-compliant CRLF sequence.
+   return $request->as_string( $CRLF );
+}
+
+# For passing in to constructor
+sub on_read
+{
+   my $self = shift;
+
+   croak "Spurious on_read of connection while idle\n";
+}
+
+# Override
+sub request
+{
+   my $self = shift;
+   my %args = @_;
+
+   my $on_response = $args{on_response} or croak "Expected 'on_response' as a CODE ref";
+   my $on_error    = $args{on_error}    or croak "Expected 'on_error' as a CODE ref";
+   
+   my $req = $args{request};
+   ref $req and $req->isa( "HTTP::Request" ) or croak "Expected 'request' as a HTTP::Request reference";
+
+   my $method = $req->method;
+
+   if( $method eq "POST" or $method eq "PUT" or length $req->content ) {
+      $req->init_header( "Content-Length", length $req->content );
+   }
+
+   my $on_read = sub {
+      my ( $self, $buffref, $closed ) = @_;
+
+      unless( $$buffref =~ s/^(.*?$CRLF$CRLF)//s ) {
+         $on_error->( "Connection closed while awaiting header" ) if $closed;
+         return 0;
+      }
+
+      my $response_header = $1;
+      my $response = HTTP::Response->parse( $response_header );
+
+      my $code = $response->code;
+
+      # RFC 2616 says "HEAD" does not have a body, nor do any 1xx codes, nor
+      # 204 (No Content) nor 304 (Not Modified)
+      if( $method eq "HEAD" or $code =~ m/^1..$/ or $code eq "204" or $code eq "304" ) {
+         $on_response->( $response );
+         return undef; # Finished
+      }
+
+      my $transfer_encoding = $response->header( "Transfer-Encoding" );
+      my $content_length = $response->content_length;
+
+      if( defined $transfer_encoding and $transfer_encoding eq "chunked" ) {
+         my $chunk_length;
+
+         return sub {
+            my ( $self, $buffref, $closed ) = @_;
+
+            if( !defined $chunk_length and $$buffref =~ s/^(.*?)$CRLF// ) {
+               # Chunk header
+               $chunk_length = hex( $1 );
+               return 1 if $chunk_length;
+
+               $on_response->( $response );
+               return undef; # Finished
+            }
+
+            if( defined $chunk_length and length( $$buffref ) >= $chunk_length ) {
+               # Chunk body
+               my $chunk = substr( $$buffref, 0, $chunk_length, "" );
+               undef $chunk_length;
+
+               $response->add_content( $chunk );
+
+               return 1;
+            }
+
+            $on_error->( "Connection closed while awaiting chunk" ) if $closed;
+            return 0;
+         };
+      }
+      elsif( defined $content_length ) {
+         if( $content_length == 0 ) {
+            $on_response->( $response );
+            return undef; # Finished
+         }
+
+         return sub {
+            my ( $self, $buffref, $closed ) = @_;
+
+            if( length $$buffref >= $content_length ) {
+               my $content = substr( $$buffref, 0, $content_length, "" );
+
+               $response->content( $content );
+
+               $on_response->( $response );
+               return undef; # Finished
+            }
+
+            $on_error->( "Connection closed while awaiting body" ) if $closed;
+            return 0;
+         };
+      }
+      else {
+         return sub {
+            my ( $self, $buffref, $closed ) = @_;
+
+            return 0 unless $closed;
+
+            my $content = $$buffref;
+            $$buffref = "";
+
+            $response->content( $content );
+
+            $on_response->( $response );
+            # $self already closed
+            return undef;
+         };
+      }
+   };
+
+   $self->SUPER::request(
+      request => $req,
+      on_read => $on_read,
+   );
+}
