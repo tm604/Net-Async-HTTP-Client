0a2a6977c8c04e9f8810084442cc50e827362363
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2009-08-31 18:08:56 +0100

    Created a simple GET example script

---
+++ examples/GET.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+
+use URI;
+
+use IO::Async::Loop;
+use Net::Async::HTTP;
+
+my $loop = IO::Async::Loop->new();
+
+my $ua = Net::Async::HTTP->new( loop => $loop );
+
+$ua->do_request(
+   method => "GET",
+   uri    => URI->new( $ARGV[0] ),
+
+   on_response => sub {
+      my ( $response ) = @_;
+
+      print $response->as_string;
+      $loop->loop_stop;
+   },
+
+   on_error => sub {
+      my ( $message ) = @_;
+
+      print STDERR "Failed - $message\n";
+      $loop->loop_stop;
+   }
+);
+
+$loop->loop_forever();
