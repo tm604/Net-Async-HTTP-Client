42a0aec375a7510b4802e6ff128eb084faddc643
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2011-01-01 18:30:23 +0000

    Bumped version to 0.09; updated Changes notes

--- lib/Net/Async/HTTP.pm
+++ lib/Net/Async/HTTP.pm
@@ -9,7 +9,7 @@ use strict;
 use warnings;
 use base qw( IO::Async::Notifier );
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 our $DEFAULT_UA = "Perl + " . __PACKAGE__ . "/$VERSION";
 our $DEFAULT_MAXREDIR = 3;
--- lib/Net/Async/HTTP/Protocol.pm
+++ lib/Net/Async/HTTP/Protocol.pm
@@ -8,7 +8,7 @@ package Net::Async::HTTP::Protocol;
 use strict;
 use warnings;
 
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
 use Carp;
 
