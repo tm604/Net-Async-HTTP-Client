ea88e34def568353fbde0942f5b16352fa5db1d3
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2013-06-19 19:02:34 +0100

    Bumped version to 0.23; updated Changes notes

--- lib/Net/Async/HTTP.pm
+++ lib/Net/Async/HTTP.pm
@@ -9,7 +9,7 @@ use strict;
 use warnings;
 use base qw( IO::Async::Notifier );
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 our $DEFAULT_UA = "Perl + " . __PACKAGE__ . "/$VERSION";
 our $DEFAULT_MAXREDIR = 3;
