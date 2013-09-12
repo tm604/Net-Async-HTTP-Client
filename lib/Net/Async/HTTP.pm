3c3a507b48f6a9af99ce0129c69cb073b0c73a59
Paul LeoNerd Evans <leonerd@leonerd.org.uk>
2013-09-12 18:44:39 +0100

    Bumped version to 0.27_002; updated Changes notes

--- lib/Net/Async/HTTP.pm
+++ lib/Net/Async/HTTP.pm
@@ -9,7 +9,7 @@ use strict;
 use warnings;
 use base qw( IO::Async::Notifier );
 
-our $VERSION = '0.27_001';
+our $VERSION = '0.27_002';
 
 our $DEFAULT_UA = "Perl + " . __PACKAGE__ . "/$VERSION";
 our $DEFAULT_MAXREDIR = 3;
--- lib/Net/Async/HTTP/Connection.pm
+++ lib/Net/Async/HTTP/Connection.pm
@@ -8,7 +8,7 @@ package Net::Async::HTTP::Connection;
 use strict;
 use warnings;
 
-our $VERSION = '0.27_001';
+our $VERSION = '0.27_002';
 
 use Carp;
 
